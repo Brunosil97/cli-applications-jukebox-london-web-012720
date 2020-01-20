e3178ef2c25e71c8aeb4b293122923afaa846156
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-20 14:58:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ def play(songs)
   user_response = gets.strip 
   if user_response.to_i >= 1 && user_response.to_i <= songs.length
     puts "Playing #{songs[user_response.to_i-1]}"
-    elsif 
+    elsif songs.include? 
 end 
 
 def exit_jukebox
