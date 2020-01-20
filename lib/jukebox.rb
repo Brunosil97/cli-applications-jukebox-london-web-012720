8982c49ea889b92aede89717b2e39d91d33196b6
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-20 14:56:22 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ def list(songs)
 def play(songs)
   puts "Please enter a song name or number:"
   user_response = gets.strip 
-  if user_response.to_i >= 
+  if user_response.to_i >= 1 && 
 end 
 
 def exit_jukebox
